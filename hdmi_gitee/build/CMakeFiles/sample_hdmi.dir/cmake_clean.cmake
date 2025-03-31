1bad15f28e26e513e380d2c88af958bd6f9fdb50
hehehe-449 <60164849+hehehe-449@users.noreply.github.com>
2025-03-31 11:43:48 +0800

    beifen

---
+++ hdmi_gitee/build/CMakeFiles/sample_hdmi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_hdmi.dir/hi_mpi_vo.c.o"
+  "CMakeFiles/sample_hdmi.dir/hi_mpi_vo.c.o.d"
+  "CMakeFiles/sample_hdmi.dir/sample_hdmi.c.o"
+  "CMakeFiles/sample_hdmi.dir/sample_hdmi.c.o.d"
+  "sample_hdmi"
+  "sample_hdmi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sample_hdmi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
